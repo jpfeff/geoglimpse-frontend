677a86e4eb5e64ea3148e4075b53df2c4b52f491
Joshua Pfefferkorn <77121865+jpfeff@users.noreply.github.com>
2024-03-10 21:25:28 -0400

    update to deployed backend URL

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
-const SERVER_URL = 'http://localhost:8000';
+// const SERVER_URL = 'http://localhost:8000';
+
+const SERVER_URL = 'https://geoglimpse-backend-r6sn.onrender.com';
 
 export default SERVER_URL;
